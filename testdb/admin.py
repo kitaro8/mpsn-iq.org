9bad14eaff883efbcecdcaac5964398051169c3f
Essa Ahmed <65098575+kitaro8@users.noreply.github.com>
2023-07-02 05:44:18 +0300

    asda

---
+++ testdb/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post, Focal, Report, Paper
+
+admin.site.register(Post)
+admin.site.register(Focal)
+admin.site.register(Report)
+admin.site.register(Paper)
