1ffacdb66f2b18ceea1b998bd1d56a00a892070d
Essa Ahmed <65098575+kitaro8@users.noreply.github.com>
2023-07-04 21:35:27 +0300

    sta

--- testdb/models.py
+++ testdb/models.py
@@ -90,3 +90,19 @@ class Paper(models.Model):
 
     def get_absolute_url(self):
         return reverse('paper', kwargs={'pk': self.pk})
+    
+class stations(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='stations')
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-date_posted']
+
+    def __str__(self):
+        return str(self.title)
+
+    def get_absolute_url(self):
+        return reverse('stations', kwargs={'pk': self.pk})
+
