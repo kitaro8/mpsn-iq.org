77f4504a758a99ce630d082de06c44d4e91f62bf
kitaro8 <kitaro.essa@yahoo.com>
2024-03-11 18:03:51 +0300

    data222

--- testdb/models.py
+++ testdb/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     event = models.CharField(max_length=100,)
     Gap = models.CharField(max_length=100)
     Magnitude = models.CharField(max_length=100)
-    # MagnitudeMW = models.CharField(max_length=100, blank=True)
+    MagnitudeMW111 = models.CharField(max_length=100, blank=True)
     Region = models.CharField(max_length=100)
     image = models.ImageField(default='default1.jpg', upload_to='image_maps', blank=True)
     image2 = models.ImageField(default='default1.jpg', upload_to='image_ml', blank=True)
