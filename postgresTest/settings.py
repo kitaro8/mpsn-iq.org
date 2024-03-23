4d5acd3e47b90303fce970af2782ed7a4feeb4e9
kitaro8 <kitaro.essa@yahoo.com>
2024-03-23 14:52:18 +0300

    'data'

--- postgresTest/settings.py
+++ postgresTest/settings.py
@@ -89,25 +89,25 @@ DATABASES = {
         'PASSWORD': 'lv94lwsO8qQ7AZy80YXAwWAgx8vq8TOx',
         'HOST': 'dpg-cigcoud9aq012ev1fn0g-a',
         'PORT': '5432',
-        # 'OPTIONS': {
-        #     'sslmode': 'require',
-        #     'sslrootcert': '/path/to/certificate.crt',
+        'OPTIONS': {
+            'sslmode': 'require',
+            'sslrootcert': '/path/to/certificate.crt',
 
-        # },
+        },
     }
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres', 
-#         'USER': 'postgres', 
-#         'PASSWORD': 'k8k8k8k8',
-#         'HOST': 'localhost', 
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres', 
+        'PASSWORD': 'k8k8k8k8',
+        'HOST': 'localhost', 
+        'PORT': '5432',
+    }
+}
 
 SECURE_SSL_CIPHERS = 'HIGH:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK'
 SECURE_SSL_PROTOCOLS = ['TLSv1.2', 'TLSv1.3']
