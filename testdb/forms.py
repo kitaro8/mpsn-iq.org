80854c1eb38f5c9d774e30f58566a615a89087be
kitaro1997 <kitaroasda@gmail.com>
2024-07-01 03:40:43 +0300

    asda

--- testdb/forms.py
+++ testdb/forms.py
@@ -23,13 +23,12 @@ class ImageForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ['image']
+
+
+class ImageForm2(forms.ModelForm):
+	class Meta:
+		model = Post
 		fields = ['image2']
-
-
-# class ImageForm2(forms.ModelForm):
-# 	class Meta:
-# 		model = Post
-# 		fields = ['image2']
 
 class NameForm(forms.Form):
 	Region = forms.CharField(label='Region', max_length=100)
