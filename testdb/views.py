4108b514f6a0f262b1bf0e4de7e736c94c026877
kitaro1997 <kitaroasda@gmail.com>
2024-09-18 22:31:55 +0300

    asda

--- testdb/views.py
+++ testdb/views.py
@@ -54,7 +54,7 @@ def upload(request):
 
 			upload_file2 = request.FILES['file2']
 			lines = upload_file.read()
-			lines1 = upload_file1.readlines().decode('utf-8')
+			lines1 = upload_file1.readlines()
 			lines11 = upload_file11.readlines().decode('utf-8')
 
 
